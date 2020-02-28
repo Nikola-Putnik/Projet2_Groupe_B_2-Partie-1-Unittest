e4f536e04fa2b3a52928efaffacbb6d59ee89208
Nikola Putnik <61552054+Nikola-Putnik@users.noreply.github.com>
2020-02-28 15:01:37 +0100

    Test unitaire d'essai
    Premier essai de test unitaire fonctionnel

--- Love_Test/src/TestLoveTests.py
+++ Love_Test/src/TestLoveTests.py
@@ -8,6 +8,7 @@ import CorrLoveTest as correct
 import LoveTest as student
 
 i1 = ["Kenny",20,"Bleu","Chien","Fifa","Got"]
+i2 = ["Platon",30,"Bleu","Chat","Fifa","Got"]
 
 l1 = [["Alice",20,"Bleu","Chien","Pes","Got"],
       ["Morgane",20,"Rouge","Chien","Fifa","Walking Dead"],
@@ -17,49 +18,15 @@ l1 = [["Alice",20,"Bleu","Chien","Pes","Got"],
 
 class TestLoveTest(unittest.TestCase):
 
-    def test0_None(self):
-        args = [-3, 0, 5]
-        rep = _("Votre fonction a retourné None pour {} comme argument. Cela implique probablement qu'il manque un return dans votre code.")
-        for n in args:
-            try:
-                student_ans = student.abs(n)
-            except Exception as e:
-                self.fail("Votre fonction a provoqué l'exception {}: {} avec comme argument {}".format(type(e), e, n))
-            self.assertIsNotNone(student_ans, rep.format(n))
-
-    def test1_abs_0(self):
-        args = [0]
-        rep = _("Votre fonction a retourné {} lorsqu'elle est appelée avec {} comme argument alors que la réponse attendue est {}")
-        for n in args:
-            try:
-                student_ans = student.abs(n)
-            except Exception as e:
-                self.fail("Votre fonction a provoqué l'exception {}: {} avec comme argument {}".format(type(e), e, n))
-            correct_ans = correct.abs(n)
-            self.assertEqual(student_ans, correct_ans,
-                             rep.format(student_ans, n, correct_ans))
-
-    def test1_abs_pos(self):
-        args = [1, 5, 17, 98]
+    def test1_LoveTest(self):
+        args = [[i1,l1], [i2,l1]]
         rep = _("Votre fonction a retourné {} lorsqu'elle est appelée avec {} comme argument positif alors que la réponse attendue est {}")
         for n in args:
             try:
-                student_ans = student.abs(n)
+                student_ans = student.love_test(n[0],n[1])
             except Exception as e:
                 self.fail("Votre fonction a provoqué l'exception {}: {} avec comme argument {}".format(type(e), e, n))
-            correct_ans = correct.abs(n)
-            self.assertEqual(student_ans, correct_ans,
-                             rep.format(student_ans, n, correct_ans))
-
-    def test2_abs_neg(self):
-        args = [-1, -9, -22, -1234]
-        rep = _("Votre fonction a retourné {} lorsqu'elle est appelée avec {} comme argument négatif alors que la réponse attendue est {}")
-        for n in args:
-            try:
-                student_ans = student.abs(n)
-            except Exception as e:
-                self.fail("Votre fonction a provoqué l'exception {}: {} avec comme argument {}".format(type(e), e, n))
-            correct_ans = correct.abs(n)
+            correct_ans = correct.love_test(n)
             self.assertEqual(student_ans, correct_ans,
                              rep.format(student_ans, n, correct_ans))
 
