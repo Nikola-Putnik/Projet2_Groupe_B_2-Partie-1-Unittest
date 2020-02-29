c58ed09089db69121a18d812fab3a9bf0dbd5a6c
Nikola Putnik <61552054+Nikola-Putnik@users.noreply.github.com>
2020-02-29 17:10:50 +0100

    correction mineure + ajout de 2 conditions
    - ajout de 2 conditions afin de toujours retourner une liste vide si la liste des candidates est vide ou si aucune n'est gagnante.
    - modification du nom de certaines variables.

--- Love_Test/src/CorrLoveTest.py
+++ Love_Test/src/CorrLoveTest.py
@@ -7,36 +7,38 @@ def love_test(jul, candidates):
     @pre <à compléter>
     @post <à compléter>
     """
-    best_femme = []
+    best_candidates = []
     best_pourcentage = 0
-    for femme in candidates:
+    for candidate in candidates:
         nbr_caract = len(jul)-1
         caract_communes = 0
         #Age
-        if femme[1] == jul[1]:
+        if candidate[1] == jul[1]:
             caract_communes += 1
         #Couleur
-        if femme[2] == jul[2]:
+        if candidate[2] == jul[2]:
             caract_communes += 1
         #Chien/Chat
-        if femme[3] == jul[3]:
+        if candidate[3] == jul[3]:
             caract_communes += 1
         #Série
-        if femme[4] == jul[4]:
+        if candidate[4] == jul[4]:
             caract_communes += 1
         #PS/Fifa
-        if femme[5] == jul[5]:
+        if candidate[5] == jul[5]:
             caract_communes += 1
         pourcentage = (caract_communes/nbr_caract)*100
-        if len(best_femme) == 0:
-            best_femme.append(femme[0])
-            best_pourcentage = pourcentage
-        else:
-            if pourcentage == best_pourcentage:
-                best_femme.append(femme[0])
-            elif pourcentage > best_pourcentage:
+        # print(candidate[0] + str(pourcentage))
+        if caract_communes > 0:
+            if len(best_candidates) == 0:
+                best_candidates.append(candidate[0])
                 best_pourcentage = pourcentage
-                best_femme[:] = [femme[0]]
-    best_femme.append(best_pourcentage)
-    return best_femme
-
+            else:
+                if pourcentage == best_pourcentage:
+                    best_candidates.append(candidate[0])
+                elif pourcentage > best_pourcentage:
+                    best_pourcentage = pourcentage
+                    best_candidates[:] = [candidate[0]]
+    if best_pourcentage > 0:
+        best_candidates.append(best_pourcentage)
+    return best_candidates
